ca73537e9d73f12a124dcdbdc114067451b63484
misheck patrick <my.github.email@misheck.wachiuri@student.moringaschool.com>
2023-07-21 14:36:32 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from "react";
+import Keypad from "./Keypad";
+ const EyesOnMe=()=>{
+     const handleFocus=()=>{
+         console.log("Good !")
+     }
+     const handleBlur=()=>{
+         console.log("Hey Eyes on me!")
+     }
+     return(
+         <div>
+        <Keypad
+            { <button onFocus={handleFocus} onBlur={handleBlur} > Eyes on me </button> }
+        />
+
+         </div>
+     )
+ }
+ export default EyesOnMe
